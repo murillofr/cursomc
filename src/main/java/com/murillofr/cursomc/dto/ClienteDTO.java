140f4ebf1e82329fb3491a202919816e0a8b16e0
murillofr <murillo.falcao@gmail.com>
2017-10-18 08:34:40 -0200

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/murillofr/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/murillofr/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.murillofr.cursomc.domain.Cliente;
+import com.murillofr.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
