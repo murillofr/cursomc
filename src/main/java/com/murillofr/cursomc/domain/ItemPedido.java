b86e7aca2824d7da73f335c598cf31163866e23b
murillofr <murillo.falcao@gmail.com>
2017-10-19 22:33:54 -0200

    Inserindo Pedido

--- src/main/java/com/murillofr/cursomc/domain/ItemPedido.java
+++ src/main/java/com/murillofr/cursomc/domain/ItemPedido.java
@@ -19,7 +19,7 @@ public class ItemPedido implements Serializable {
 	private Integer quantidade;
 	private Double preco;
 	
-	private ItemPedido() {
+	public ItemPedido() {
 	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -40,8 +40,16 @@ public class ItemPedido implements Serializable {
 		return id.getPedido();
 	}
 	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
 	public Produto getProduto() {
 		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
 	}
 	
 	public ItemPedidoPK getId() {
