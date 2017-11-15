2774d1aa2cdcddb46e9d970bb7f9869ac39b29a0
murillofr <murillo.falcao@gmail.com>
2017-11-15 11:35:53 -0200

    Esqueci a senha

--- src/main/java/com/murillofr/cursomc/services/EmailService.java
+++ src/main/java/com/murillofr/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.murillofr.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.murillofr.cursomc.domain.Cliente;
 import com.murillofr.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
